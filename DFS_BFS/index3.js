fb752e0f48c79c177edad15c368c5a76c5178173
jjsh03 <aberrant03@gmail.com>
2023-11-29 04:09:28 +0900

    study: bfs 주석 설명 추가

--- DFS_BFS/index3.js
+++ DFS_BFS/index3.js
@@ -1,18 +1,25 @@
 // 4. 이진트리_BFS
 function BFS() {
-  let answer = '';
-  let queue = [];
-  queue.push(1);
+  let answer = ''; // BFS 결과를 저장할 변수
+  let queue = []; // BFS에서 사용할 큐
+  queue.push(1); // 초기값으로 루트 노드인 1을 큐에 삽입
+
+  // 큐가 비어있을 때까지 반복
   while (queue.length) {
     console.log(queue);
-    let v = queue.shift();
-    answer += v + ' ';
+
+    let v = queue.shift(); // 큐에서 첫 번째 원소를 추출하여 v에 할당
+    answer += v + ' '; // 결과에 현재 노드 값을 추가
+
+    // 현재 노드의 자식 노드들을 큐에 삽입
     for (let nv of [v * 2, v * 2 + 1]) {
-      if (nv > 7) continue;
+      // nv는 현재 노드의 자식 노드(v의 왼쪽 자식: v * 2, 오른쪽 자식: v * 2 +1)
+      if (nv > 7) continue; // 트리의 노드 수가 7이므로 7보다 큰 경우 무시
       queue.push(nv);
     }
   }
-  return answer;
+
+  return answer; // BFS 결과 반환
 }
 
 console.log(BFS());
@@ -20,50 +27,64 @@ console.log(BFS());
 // 5_1. 송아지찾기
 function solution(s, e) {
   let answer = 0;
-  let ch = Array.from({ length: 10001 }, () => 0);
-  let dis = Array.from({ length: 10001 }, () => 0);
+  let ch = Array.from({ length: 10001 }, () => 0); // 방문 여부를 체크하는 배열
+  let dis = Array.from({ length: 10001 }, () => 0); // 출발 지점부터의 이동 거리를 저장하는 배열
   let queue = [];
-  queue.push(s);
-  ch[s] = 1;
-  dis[s] = 0;
+  queue.push(s); // 초기값으로 출발 지점을 큐에 삽입
+  ch[s] = 1; // 출발 지점을 방문했음을 표시
+  dis[s] = 0; // 출발 지점까지의 이동 거리는 0
+
   while (queue.length) {
-    let x = queue.shift();
+    let x = queue.shift(); // 큐에서 현재 위치를 추출
+
+    // 현재 위치에서 이동 가능한 다음 위치들을 확인
     for (let nx of [x - 1, x + 1, x + 5]) {
+      // 다음 위치가 목표 지점인 경우, 최소 이동 횟수를 반환
       if (nx === e) return dis[x] + 1;
+
+      // 다음 위치가 유효하고 아직 방문하지 않은 경우
       if (nx > 0 && nx <= 10000 && ch[nx] === 0) {
-        ch[nx] = 1;
-        queue.push(nx);
-        dis[nx] = dis[x] + 1;
+        ch[nx] = 1; // 다음 위치를 방문했음을 표시
+        queue.push(nx); // 큐에 다음 위치를 삽입
+        dis[nx] = dis[x] + 1; // 출발 지점으로부터의 이동 거리를 갱신
       }
     }
   }
+
   return answer;
 }
 
 console.log(solution(8, 3));
 
 // 5_2. 송아지찾기_레벨사용
+// 각 레벨에서의 이동 횟수 탐색 가능
 function solution1(s, e) {
   let answer = 0;
   let ch = Array.from({ length: 10001 }, () => 0);
   let queue = [];
   queue.push(s);
   ch[s] = 1;
-  let L = 0;
+  let L = 0; // 레벨을 나타내는 변수
+
   while (queue.length) {
     let len = queue.length;
+
+    // 현재 레벨의 노드들에 대해 탐색
     for (let i = 0; i < len; i++) {
       let x = queue.shift();
+
+      // 현재 노드에서 이동 가능한 다음 위치들을 확인
       for (let nx of [x - 1, x + 1, x + 5]) {
-        if (nx === e) return L + 1;
+        if (nx === e) return L + 1; // 목표 지점에 도달하면 레벨을 반환
         if (nx > 0 && nx <= 10000 && ch[nx] === 0) {
           ch[nx] = 1;
           queue.push(nx);
         }
       }
     }
-    L++;
+    L++; // 다음 레벨로 이동
   }
+
   return answer;
 }
 
--- DFS_BFS/index4.js
+++ DFS_BFS/index4.js
@@ -5,17 +5,25 @@ function solution(board) {
   let dx = [-1, -1, 0, 1, 1, 1, 0, -1];
   let dy = [0, 1, 1, 1, 0, -1, -1, -1];
   let queue = [];
+
+  // 모든 좌표를 순회
   for (let i = 0; i < n; i++) {
     for (let j = 0; j < n; j++) {
+      // 현재 좌표가 육지(1)라면 DFS 시작
       if (board[i][j] === 1) {
-        board[i][j] = 0;
+        board[i][j] = 0; // 방문한 육지를 0으로 변경
         queue.push([i, j]);
         answer++;
+
+        // BFS를 통해 현재 육지와 연결된 모든 육지를 방문
         while (queue.length) {
           let x = queue.shift();
           for (let k = 0; k < 8; k++) {
+            // 대각선 포함 8방향
+            // 현재 위치 + k = 다음 위치
             let nx = x[0] + dx[k];
             let ny = x[1] + dy[k];
+            // 해당 위치가 육지인지 확인
             if (nx >= 0 && nx < n && ny >= 0 && ny < n && board[nx][ny] === 1) {
               board[nx][ny] = 0;
               queue.push([nx, ny]);
@@ -40,14 +48,16 @@ let arr = [
 
 console.log(solution(arr));
 
-// 6_2. 섬나라 아일랜드_BFS
+// 6_2. 섬나라 아일랜드_DFS
 function solution1(board) {
   let answer = 0;
   let n = board.length;
   let dx = [-1, -1, 0, 1, 1, 1, 0, -1];
   let dy = [0, 1, 1, 1, 0, -1, -1, -1];
+
+  // DFS 함수 정의
   function DFS(x, y) {
-    board[x][y] = 0;
+    board[x][y] = 0; // 방문한 육지를 0으로 변경
     for (let k = 0; k < 8; k++) {
       let nx = x + dx[k];
       let ny = y + dy[k];
@@ -56,12 +66,15 @@ function solution1(board) {
       }
     }
   }
+
+  // 모든 좌표를 순회
   for (let i = 0; i < n; i++) {
     for (let j = 0; j < n; j++) {
+      // 현재 좌표가 육지(1)라면 DFS 시작
       if (board[i][j] === 1) {
-        board[i][j] = 0;
+        board[i][j] = 0; // 방문한 육지를 0으로 변경
         answer++;
-        DFS(i, j);
+        DFS(i, j); // DFS를 통해 현재 육지와 연결된 모든 육지를 방문
       }
     }
   }
